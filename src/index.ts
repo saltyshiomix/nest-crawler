61fc800af6944a7038c047c1da1eec1cbbcb434d
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-08-23 22:58:09 +0900

    Use @web-master/node-web-fetch instead of scrape-it

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
+import {
+  FetchResult,
+  CrawlerConfig,
+  ScraperConfig,
+} from '@web-master/node-web-fetch';
 import { CrawlerModule } from './crawler/crawler.module';
 import { CrawlerService } from './crawler/crawler.service';
-import {
-  CrawlerConfig,
-  ScrapeOptions,
-  ScrapeOptionLinks,
-  ScrapeOptionElement,
-  ScrapeOptionList,
-  ScrapeResult,
-} from './crawler/interfaces';
 
 export {
   CrawlerModule,
   CrawlerService,
+  FetchResult,
   CrawlerConfig,
-  ScrapeOptions,
-  ScrapeOptionLinks,
-  ScrapeOptionElement,
-  ScrapeOptionList,
-  ScrapeResult,
+  ScraperConfig,
 }
