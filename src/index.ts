c7838ece8442b860de6858de6a2c7e026445e9ee
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-09-02 00:34:28 +0900

    Export useful type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {
   ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
+  CrawlLinkOptions,
 } from '@web-master/node-web-fetch';
 import { NestCrawlerModule } from './crawler.module';
 import { NestCrawlerService } from './crawler.service';
@@ -8,7 +12,11 @@ import { normalizeNumber } from './utils';
 
 export {
   ScrapeConfig,
+  ScrapeOptions,
+  ScrapeOptionElement,
+  ScrapeOptionList,
   CrawlConfig,
+  CrawlLinkOptions,
   NestCrawlerModule,
   NestCrawlerService,
   normalizeNumber,
